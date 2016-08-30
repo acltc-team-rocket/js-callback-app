3457a8ce8cca26b3a555f66ca6f4a031d0acdfe0
Mark Richardson <mark@markeban.com>
2016-08-29 20:12:37 -0700

    add get api call via jquery

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,8 +1,28 @@
-function changeColors() {
-  setTimeout(function() {
-    document.getElementById('first').style.color = "red";
-    setTimeout(function() {
-      document.getElementById('second').style.color = "blue";
-    }, 1000);
-  }, 1000);
+// function changeColors() {
+//   setTimeout(function() {
+//     document.getElementById('first').style.color = "red";
+//     setTimeout(function() {
+//       document.getElementById('second').style.color = "blue";
+//       setTimeout(function() {
+//         document.getElementById('third').style.fontSize = "2.0em";
+//         setTimeout(function() {
+//           document.getElementById('fourth').style.background = "yellow";
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }
+function getData() {
+  var divFoodtrucks = document.getElementById("foodtrucks");
+  divFoodtrucks.innerHTML = "Loading...";
+
+  $.get('https://data.sfgov.org/resource/jjew-r69b.json', function(response) {
+    var htmlString = ""
+    for (var i = 0; i < response.length; i++) {
+      var foodtruck = response[i];
+      htmlString += '<h2>' + foodtruck.applicant + '</h2>'
+      htmlString += '<p>' + foodtruck.optionaltext + '</p>'
+    }
+    divFoodtrucks.innerHTML = htmlString;
+  });
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/', to: 'pages#index'
+  get '/foodtrucks', to: 'pages#foodtrucks'
 end
